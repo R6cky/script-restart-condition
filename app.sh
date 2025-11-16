e9202efb9788015e5d42ca816cd5925a8420462e
r6cky <cleitonrocha321@hotmail.com>
2025-11-16 19:39:02 -0300

    add comment in code

--- app.sh
+++ app.sh
@@ -9,17 +9,17 @@ CPU_USO=$(top -bn1 | grep -i "Cpu(s)" | awk '{print 100 - $8}')
 CPU_USO=${CPU_USO%.*}
 
 # Coletar uso de memória
-MEM_USO=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
+#MEM_USO=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
 
-echo "CPU: $CPU_USO% | MEM: $MEM_USO%"
+#echo "CPU: $CPU_USO% | MEM: $MEM_USO%"
 
 # Verifica limites
-if [ "$CPU_USO" -ge "$CPU_LIMITE" ]; then
-    echo "CPU acima de $CPU_LIMITE%, reiniciando..."
-    /usr/sbin/reboot
-fi
+#if [ "$CPU_USO" -ge "$CPU_LIMITE" ]; then
+#    echo "CPU acima de $CPU_LIMITE%, reiniciando..."
+#    /usr/sbin/reboot
+#fi
 
-if [ "$MEM_USO" -ge "$MEM_LIMITE" ]; then
-    echo "Memória acima de $MEM_LIMITE%, reiniciando..."
-    /usr/sbin/reboot
-fi
+#if [ "$MEM_USO" -ge "$MEM_LIMITE" ]; then
+#    echo "Memória acima de $MEM_LIMITE%, reiniciando..."
+#    /usr/sbin/reboot
+#fi

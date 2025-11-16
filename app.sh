03f0093341d022eb7410a1bbab0a143e4c6c8b63
r6cky <cleitonrocha321@hotmail.com>
2025-11-16 19:41:49 -0300

    include echo in code, for test

--- app.sh
+++ app.sh
@@ -5,8 +5,8 @@ CPU_LIMITE=90      # em %
 MEM_LIMITE=90      # em %
 
 # Coletar uso de CPU (média de 1 minuto)
-CPU_USO=$(top -bn1 | grep -i "Cpu(s)" | awk '{print 100 - $8}')
-CPU_USO=${CPU_USO%.*}
+echo CPU_USO=$(top -bn1 | grep -i "Cpu(s)" | awk '{print 100 - $8}')
+echo CPU_USO=${CPU_USO%.*}
 
 # Coletar uso de memória
 #MEM_USO=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')

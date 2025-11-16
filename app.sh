97152e2827ed16765c1d793f5a935ed3eb683e17
r6cky <cleitonrocha321@hotmail.com>
2025-11-16 19:53:02 -0300

    remove echo in some lines

--- app.sh
+++ app.sh
@@ -6,10 +6,10 @@ MEM_LIMITE=90      # em %
 
 # Coletar uso de CPU (média de 1 minuto)
 CPU_USO=$(top -bn1 | grep -i "Cpu(s)" | awk '{print 100 - $8}')
-echo CPU_USO=${CPU_USO%.*}
+CPU_USO=${CPU_USO%.*}
 
 # Coletar uso de memória
-echo MEM_USO=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
+MEM_USO=$(free | awk '/Mem/ {printf("%.0f"), $3/$2 * 100}')
 
 echo "CPU: $CPU_USO% | MEM: $MEM_USO%"
 
